c4287aea798f00cfc23422581b0dada9b2d78031
eden <devfeijoa@gmail.com>
2024-12-10 17:46:16 +0900

    refactor: 월과 요일을 입력받는 기능과 유효성 검사 수정

--- src/main/kotlin/oncall/view/InputView.kt
+++ src/main/kotlin/oncall/view/InputView.kt
@@ -2,37 +2,31 @@ package oncall.view
 
 import camp.nextstep.edu.missionutils.Console
 import oncall.util.Validator
+import oncall.util.Validator.validateDay
+import oncall.util.Validator.validateIsDigit
+import oncall.util.Validator.validateLength
+import oncall.util.Validator.validateNumberRange
 
 class InputView {
 
     fun readMonthAndDay(): Pair<Int, String> {
         print("비상 근무를 배정할 월과 시작 요일을 입력하세요> ")
-        val input = Console.readLine()
 
         try {
-            // 입력값이 비어 있는지 확인
-            Validator.validateNotEmpty(input)
-            // 쉼표로 구분된 월과 요일인지 확인
-            Validator.validateSize(input)
-            // 쉼표를 기준으로 분리
-            val parts = input.split(",")
-            // 각 값이 비어있지 않은지 검증
-            Validator.validateEachPartNotEmpty(parts)
+            val (month, day) = Console.readLine().split(SPLITTER)
+            validateIsDigit(month)
+            validateNumberRange(month.toInt())
+            validateDay(day)
+            validateLength(day)
+            return Pair(month.toInt(), day)
 
-            //월과 요일 각각 추가 검증
-            val month = parts[0]
-            val day = parts[1]
-
-            Validator.validateIsDigit(month) // month 숫자인지 확인
-            val monthInt = month.toInt()
-            Validator.validateNumberRange(monthInt) // month 범위인지 확인
-            Validator.validateLength(day) //요일 길이 확인
-            Validator.validateDay(day) // 유효한 요일 이름인지 확인
-
-            return Pair(monthInt, day)
         } catch (e: IllegalArgumentException) {
-            println("[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요.")
-            return readMonthAndDay()
+            throw IllegalArgumentException(INVALID_INPUT)
         }
+
+
+    }
+    companion object {
+        const val SPLITTER = ","
     }
 }
