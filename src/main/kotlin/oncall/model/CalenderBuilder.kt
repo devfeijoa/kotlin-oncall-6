6f56c14ed04c9064313563bcdeb71fa981e7a353
eden <devfeijoa@gmail.com>
2024-12-11 10:23:05 +0900

    feat: calendar 만드는 기능

---
+++ src/main/kotlin/oncall/model/CalenderBuilder.kt
@@ -0,0 +1,78 @@
+package oncall.model
+
+import oncall.model.Month.Companion.getEndDate
+import oncall.model.Month.Companion.toMonth
+import java.util.LinkedList
+import java.util.Queue
+
+
+class CalenderBuilder(private val dateInfo: Pair<Int, String>) {
+    private val calendar = mutableListOf<Pair<String, Boolean>>() // 날짜 정보와 휴일 여부
+    private val dayQueue: Queue<String> = LinkedList(
+        listOf(
+            MONDAY,
+            TUESDAY,
+            WEDNESDAY,
+            THURSDAY,
+            FRIDAY,
+            SATURDAY,
+            SUNDAY
+        )
+    )
+
+    init {
+        initDayQueue(dateInfo.second)
+    }
+
+    private fun initDayQueue(targetDay: String) {
+        while (true) {
+            if (dayQueue.peek() == targetDay) break // peek()
+            rotateQueue()
+        }
+    }
+
+    private fun rotateQueue() {
+        dayQueue.add(dayQueue.poll())
+    }
+
+    fun make(): MutableList<Pair<String, Boolean>> {
+        val month = dateInfo.first.toMonth()
+        for(date in 1..getEndDate(month)) {
+            setHoliday(date, month)
+        }
+        return calendar
+    }
+
+    private fun setHoliday(date: Int, month: Month) {
+        val todayDay = dayQueue.peek()
+        when(isHoliday(month, date, todayDay)) {
+            true -> {
+                if(!isWeekend(todayDay)) todayDay += WEEKDAY_HOLIDAY_MARK
+                calendar.add(Pair(CALENDER_INFO_TEMPLATE.format(month.value, date, todayDay), true))
+            }
+            false -> calendar.add(Pair(CALENDER_INFO_TEMPLATE.format(month.value, date, todayDay), false))
+        }
+        rotateQueue()
+    }
+
+    private fun isHoliday(month: Month, date: Int, todayDay: String):Boolean {
+        return (month.hoiliday.contains(date) || listOf(SATURDAY, SUNDAY).contains(todayDay))
+    }
+
+    private fun isWeekend(todayDay: String): Boolean {
+        return listOf(SATURDAY, SUNDAY).contains(todayDay)
+    }
+
+    companion object {
+        const val WEEKDAY_HOLIDAY_MARK = "(휴일)"
+        const val CALENDER_INFO_TEMPLATE = "%s월 %s일 %s"
+
+        const val MONDAY = "월"
+        const val TUESDAY = "화"
+        const val WEDNESDAY = "수"
+        const val THURSDAY = "목"
+        const val FRIDAY = "금"
+        const val SATURDAY = "토"
+        const val SUNDAY = "일"
+    }
+}
